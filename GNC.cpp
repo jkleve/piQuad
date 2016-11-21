262f9ec5eb670d1858f86788fcc39e1e56ca01f1
Jesse Kleve <jesse.kleve@gmail.com>
2016-11-20 19:18:14 -0600

    moved mpu stuff to new GNC files

---
+++ GNC.cpp
@@ -0,0 +1,71 @@
+
+
+#include "GNC.h"
+
+GNC::GNC()
+{
+    // initialize device
+    printf("Initializing I2C devices...\n");
+    mpu.initialize();
+
+    // verify connection
+    printf("Testing device connections...\n");
+    printf(mpu.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
+
+    // load and configure the DMP
+    printf("Initializing DMP...\n");
+    devStatus = mpu.dmpInitialize();
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        printf("Enabling DMP...\n");
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        //attachInterrupt(0, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        printf("DMP ready!\n");
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        printf("DMP Initialization failed (code %d)\n", devStatus);
+    }
+}
+
+GNC::~GNC()
+{
+}
+
+GNC::step()
+{
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    if (fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        printf("FIFO overflow!\n");
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (fifoCount >= 42) {
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+        // display quaternion values in easy matrix form: w x y z
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        printf("quat %7.2f %7.2f %7.2f %7.2f    ", q.w,q.x,q.y,q.z);
+        printf("\n");
+    }
+}
