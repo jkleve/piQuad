bc3a7343bc00caf6f7bf7dce96da3f3d2a2719ea
Jesse Kleve <jesse.kleve@gmail.com>
2016-11-20 18:33:27 -0600

    removed unneeded things from makefile and changed the main function that is being used

--- Makefile
+++ Makefile
@@ -1,16 +1,15 @@
 # You need to "sudo apt-get install libgtkmm-3.0-dev" to build the demo_3d binary
 
-#all: demo_raw demo_dmp demo_3d
 all: demo_dmp
 
 HDRS = helper_3dmath.h I2Cdev.h MPU6050_6Axis_MotionApps20.h MPU6050.h
 CMN_OBJS = I2Cdev.o MPU6050.o
-DMP_OBJS = demo_dmp.o
+DMP_OBJS = main.o
 
 # Set DMP FIFO rate to 20Hz to avoid overflows on 3d demo.  See comments in
 # MPU6050_6Axis_MotionApps20.h for details.
 
-CXXFLAGS = -DDMP_FIFO_RATE=9 -Wall -g -O2 `pkg-config gtkmm-3.0 --cflags --libs`
+CXXFLAGS = -DDMP_FIFO_RATE=9 -Wall -g -O2
 
 $(CMN_OBJS) $(DMP_OBJS) $(RAW_OBJS) : $(HDRS)
 
@@ -19,4 +18,3 @@ demo_dmp: $(CMN_OBJS) $(DMP_OBJS)
 
 clean:
 	rm -f $(CMN_OBJS) $(DMP_OBJS) demo_dmp
-
--- demo_dmp.cpp
+++ demo_dmp.cpp
@@ -22,14 +22,14 @@ MPU6050 mpu;
 // (in degrees) calculated from the quaternions coming from the FIFO.
 // Note that Euler angles suffer from gimbal lock (for more info, see
 // http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_EULER
 
 // uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
 // pitch/roll angles (in degrees) calculated from the quaternions coming
 // from the FIFO. Note this also requires gravity vector calculations.
 // Also note that yaw/pitch/roll angles suffer from gimbal lock (for
 // more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-#define OUTPUT_READABLE_YAWPITCHROLL
+//#define OUTPUT_READABLE_YAWPITCHROLL
 
 // uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
 // components with gravity removed. This acceleration reference frame is
@@ -85,7 +85,7 @@ void setup() {
     // load and configure the DMP
     printf("Initializing DMP...\n");
     devStatus = mpu.dmpInitialize();
-    
+
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
         // turn on the DMP, now that it's ready
@@ -132,7 +132,7 @@ void loop() {
     } else if (fifoCount >= 42) {
         // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
+
         #ifdef OUTPUT_READABLE_QUATERNION
             // display quaternion values in easy matrix form: w x y z
             mpu.dmpGetQuaternion(&q, fifoBuffer);
@@ -172,7 +172,7 @@ void loop() {
             mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
             printf("aworld %6d %6d %6d    ", aaWorld.x, aaWorld.y, aaWorld.z);
         #endif
-    
+
         #ifdef OUTPUT_TEAPOT
             // display quaternion values in InvenSense Teapot demo format:
             teapotPacket[2] = fifoBuffer[0];
@@ -198,4 +198,3 @@ int main() {
 
     return 0;
 }
-
