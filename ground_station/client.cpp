9aedd982d94dcf642b13516b20b7fc11334ba2f1
Jesse Kleve <jesse.kleve@gmail.com>
2016-12-28 09:48:02 -0600

    updated client

--- ground_station/client.cpp
+++ ground_station/client.cpp
@@ -1,51 +1,99 @@
 //
-// client.cpp
-// ~~~~~~~~~~
+// blocking_udp_echo_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
+#include <algorithm>
+#include <ctime>
+#include <string>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
-#include <boost/array.hpp>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+
+#include "../drone/comms.h"
 
 using boost::asio::ip::udp;
+
+enum { max_length = 1024 };
+
+std::string make_daytime_string()
+{
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
 
 int main(int argc, char* argv[])
 {
   try
   {
-    if (argc != 2)
-    {
-      std::cerr << "Usage: client <host>" << std::endl;
-      return 1;
-    }
+
+    //std::string addr = "localhost";
+    std::string addr = "192.168.0.29";
+    std::string port = "3643";
 
     boost::asio::io_service io_service;
+    char reply[max_length];
+    udp::endpoint sender_endpoint;
+
+    udp::socket s(io_service, udp::endpoint(udp::v4(), 0));
 
     udp::resolver resolver(io_service);
-    udp::resolver::query query(udp::v4(), argv[1], "daytime");
-    udp::endpoint receiver_endpoint = *resolver.resolve(query);
+    udp::endpoint endpoint = *resolver.resolve({udp::v4(), addr, port});
 
-    udp::socket socket(io_service);
-    socket.open(udp::v4());
+    // connect
+    //std::cout << "Hit enter to connect";
+    //std::cin.getline(reply, max_length);
+    //std::cout << std::endl;
+    char request[max_length] = "Request Connection.\0";
+    size_t request_length = std::strlen(request);
+    // s.send_to(boost::asio::buffer(request, request_length), endpoint); TODO removed this because we need to fix server to accept the initial connect request
 
-    boost::array<char, 1> send_buf  = { 0 };
-    socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
+    // receive confirmation
+    // size_t reply_length = s.receive_from(
+    //     boost::asio::buffer(reply, max_length), sender_endpoint);
+    // std::cout << "Connected: ";
+    // std::cout.write(reply, reply_length);
+    // std::cout << std::endl;
 
-    boost::array<char, 128> recv_buf;
-    udp::endpoint sender_endpoint;
-    size_t len = socket.receive_from(
-        boost::asio::buffer(recv_buf), sender_endpoint);
+    int i = 0;
 
-    std::cout.write(recv_buf.data(), len);
+    while (true)
+    {
+        // send message
+
+        const ui::data_t* d = new ui::data_t(ui::none, ++i, 0, 0, 0);
+        const void* v = d;
+
+        //std::string* str = new std::string(make_daytime_string());
+        //str->erase(std::remove(str->begin(), str->end(), '\n'), str->end());
+        //boost::shared_ptr<std::string> message(str);
+        //std::cout << "Send message: ";
+        //std::cin.getline(reply, max_length);
+        //std::cout << std::endl;
+        std::cout << "Sending message ..." << std::endl;
+        //s.send_to(boost::asio::buffer(*message), endpoint);
+        s.send_to(boost::asio::buffer(v, sizeof(*d)), endpoint);
+        boost::this_thread::sleep(boost::posix_time::milliseconds(50));
+        // get response
+        // std::cout << "Waiting for response ..." << std::endl;
+        // size_t reply_length = s.receive_from(
+        //     boost::asio::buffer(reply, max_length), sender_endpoint);
+        // std::cout << "Reply is: ";
+        // std::cout.write(reply, reply_length);
+        // std::cout << "\n";
+    }
   }
   catch (std::exception& e)
   {
-    std::cerr << e.what() << std::endl;
+    std::cerr << "Exception: " << e.what() << "\n";
   }
 
   return 0;
